47c182324e795138d5f384110172a1da201a2f8a
snerix <zhangyu1119@gmail.com>
2026-01-26 23:11:17 +0800

    Initial Commit

---
+++ OfflinePasswordManager/OfflinePasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflinePasswordManagerApp.swift
+//  OfflinePasswordManager
+//
+//  Created by Yu Zhang on 2026/1/26.
+//
+
+import SwiftUI
+
+@main
+struct OfflinePasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
